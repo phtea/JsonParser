2fdd326a6072a2632806ed71a57d11e3fc7fa405
phtea <phtea>
2025-12-31 16:29:37 +0300

    designed a token

--- JsonParser/include/json/token.h
+++ JsonParser/include/json/token.h
@@ -1,1 +1,26 @@
 #pragma once
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <variant>
+
+namespace json {
+	struct Null {};
+	struct Value;
+
+	using Array = std::vector<Value>;
+	using Object = std::unordered_map<std::string, Value>;
+
+	struct  Value {
+		using Variant = std::variant<
+			Null,
+			bool,
+			double,
+			std::string,
+			Array,
+			Object
+		>;
+
+		Variant data;
+	};
+}
